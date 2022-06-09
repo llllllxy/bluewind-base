cdb19473949ea9511dc8f9b97ab8d5931e07d01e
liuxingyu01 <184974699@qq.com>
2022-06-09 16:07:09 +0800

    更新RedisConfig配置

--- src/main/java/com/bluewind/base/common/config/RedisConfig.java
+++ src/main/java/com/bluewind/base/common/config/RedisConfig.java
@@ -7,7 +7,6 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -27,7 +26,7 @@ import java.time.Duration;
  * @description redis配置类
  **/
 @Configuration
-public class RedisConfig extends CachingConfigurerSupport {
+public class RedisConfig {
     private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
 
     @Value("${redis.redisDbIndex}")
