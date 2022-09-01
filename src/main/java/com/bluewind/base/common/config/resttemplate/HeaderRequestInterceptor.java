31537054d6e105c084bf1440b925cbc9672993ac
liuxingyu01 <184974699@qq.com>
2022-09-01 13:58:13 +0800

    Optimize: 增加RestTemplate配置类

---
+++ src/main/java/com/bluewind/base/common/config/resttemplate/HeaderRequestInterceptor.java
@@ -0,0 +1,32 @@
+package com.bluewind.base.common.config.resttemplate;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+/**
+ * @author liuxingyu01
+ * @date 2022-09-01 11:42
+ * @description
+ **/
+public class HeaderRequestInterceptor implements ClientHttpRequestInterceptor {
+
+    private final String headerName;
+
+    private final String headerValue;
+
+    public HeaderRequestInterceptor(String headerName, String headerValue) {
+        this.headerName = headerName;
+        this.headerValue = headerValue;
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        request.getHeaders().set(headerName, headerValue);
+        return execution.execute(request, body);
+    }
+
+}
